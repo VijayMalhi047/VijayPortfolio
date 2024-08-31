c4fae26f7ec295f73749722f203797cf0f0da395
Vijay Kumar Malhi <116957144+VijayMalhi047@users.noreply.github.com>
2024-08-31 21:08:21 +0500

    first commit

---
+++ src/static/Summary.tsx
@@ -0,0 +1,11 @@
+
+function Summary(){
+    return (<>
+        <p className="text-4xl mb-8">About me</p>
+        <article className="desktop:text-xl mobile:text-md" itemScope itemType="https://schema.org/author">
+            <p className="mb-5">I&apos;m a third-year software engineering student and a Front-End developer. Currently, I&apos;m gaining industry experience through a fellowship at Bytewise Limited. My main focus these days is building high-performance web applications for desktop and mobile devices using the latest technologies. </p>
+        </article>
+    </>);
+}
+
+export default Summary;
